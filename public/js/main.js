230d549fc3052d97b84510189d15815de419cd83
Ethan Stiffelman <Ethan@Ethans-MacBook-Air.local>
2013-06-27 11:35:43 -0700

    set up JSON capture form

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 			var fullName = first + ' ' + last; 
 			$('#name').html(fullName);
 
-			console.log(callBack);   // LOGGING JSON response *
+			console.log(callBack);   // LOGGING JSON response obj *
 
 			var email = callBack.contact_info.email;   // CONTACT INFO *
 			var phone = callBack.contact_info.phone;
@@ -41,22 +41,57 @@ $(document).ready(function() {
 			var exp2 = callBack.experience[1].organization;
 			$('#exp1').html(exp1);
 			$('#exp2').html(exp2);
-
-
-
 		}
 	});
 	
-	$('#save_exit').click(function() {
-		console.log('Success')
+
+	$('#add_form').submit(function(){
+		var fullName = $('#signup_name').val();
+		return false;
 	});
+
+
+
+	$('#shoutForm').submit(function(event){
+		console.log(event);
+		var input = $('#sbInput').val();
+		$('#display').append(input);
+		$('#display').append(' ');
+		$('#display').append('<br>');
+		$('#sbInput').val('');
+		console.log(input + ' = Success!');
+		return false;
+	});
+
+
 
 	$('#add_form').submit(function(){
 		var userData = {};
-		userData['#signup_name'] = $('#signup_name').val();
-		console.log(userData); 
+
+		userData.contact_info = {};
+		userData.contact_info.email = $('#add_email').val();
+		userData.contact_info.phone = $('#add_phone').val();
+
+		address = {};
+		address.city = $('#add_city').val();
+		address.state = $('#add_state').val();
+		address.street = $('#add_street').val();
+		address.zip = $('#add_zip').val();
+		userData.contact_info.street_address = address;
+
+		userData.name_first = $('#add_nameFirst').val();
+		userData.name_last = $('#add_nameLast').val();
+		userData.schools = [];
+		userData.schools[0] = {};
+		userData.schools[0].name = $('#add_school').val();
+		userData.experience = [];
+		userData.experience[0] = {};
+		userData.experience[0].organization = $('#add_employer').val();
+		console.log(userData);
 		return false;
-	});
+
+
+	})
 });
 
 
